1eef62108370108b9cc3389891a05f53d70c58f8
Hebsp <hsp2@cin.ufpe.br>
2026-03-02 02:28:32 -0300

    feat(generator):add IA API integration and caching

---
+++ src/generator.py
@@ -0,0 +1,56 @@
+from prompts import gerar_prompt_exemplo, gerar_prompt_exercicio, gerar_prompt_explicacao
+from cache import buscar_na_cache, salvar_na_cache
+from google import genai
+
+def configurar_cliente(api_key):
+    """
+    Configura o cliente da API Gemini com a chave fornecida.
+    """
+    return genai.Client(api_key=api_key)
+
+def call_api(client, prompt):
+    """
+    Chama a API Gemini com o prompt gerado e retorna a resposta.
+    """
+    resposta = client.models.generate_content(
+        model='gemini-1.5-flash',
+        contents=prompt
+        )
+    return resposta.text
+
+def gerar_explicacao(client, topico, perfil):
+    """
+    Gera uma explicação conceitual usando a API.
+    """
+    cache_result = buscar_na_cache(topico, perfil, "explicacao")
+    if cache_result:
+        return cache_result
+    prompt = gerar_prompt_explicacao(topico, perfil)
+    resposta = call_api(client, prompt)
+    salvar_na_cache(topico, perfil, "explicacao", resposta)
+    return resposta
+
+
+def gerar_exemplo(client, topico, perfil):
+    """
+    Gera um exemplo prático usando a API.
+    """
+    cache_result = buscar_na_cache(topico, perfil, "exemplo")
+    if cache_result:
+        return cache_result
+    prompt = gerar_prompt_exemplo(topico, perfil)
+    resposta = call_api(client, prompt)
+    salvar_na_cache(topico, perfil, "exemplo", resposta)
+    return resposta
+
+def gerar_exercicio(client, topico, perfil):
+    """
+    Gera um exercício de fixação usando a API.
+    """
+    cache_result = buscar_na_cache(topico, perfil, "exercicio")
+    if cache_result:
+        return cache_result
+    prompt = gerar_prompt_exercicio(topico, perfil)
+    resposta = call_api(client, prompt)
+    salvar_na_cache(topico, perfil, "exercicio", resposta)
+    return resposta
