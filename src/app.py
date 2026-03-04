c41fb5ecb4a04d6fde534d4f95b42c68dc7cf38e
HebSP <hsp2@cin.ufpe.br>
2026-03-04 18:58:28 -0300

    feat(server): add Procfile and gunicorn

--- src/app.py
+++ src/app.py
@@ -123,4 +123,5 @@ def formatar_mapa(texto):
     return res
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
